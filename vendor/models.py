d8999cd78120737ca4fbdfd43bef61e349f990ba
yutaku-kim <yutaku1011@gmail.com>
2024-01-09 22:47:47 +0900

    vendor and customer dashboard edits

--- vendor/models.py
+++ vendor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User, UserProfile
+from accounts.utils import send_notification
 
 class Vendor(models.Model):
     user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE)
@@ -13,3 +14,24 @@ class Vendor(models.Model):
 
     def __str__(self):
         return self.vendor_name
+    
+    def save(self, *args, **kwargs):
+        if self.pk is not None:
+            #update
+            orig = Vendor.objects.get(pk=self.pk)
+            if orig.is_approved != self.is_approved:
+                mail_template = 'accounts/emails/admin_approval_email.html'
+                context = {
+                    'user': self.user,
+                    'is_approved': self.is_approved,
+                }
+                if self.is_approved == True:
+                    #send notification email
+                    mail_subject = 'Your restaurant has been approved'
+                    send_notification(mail_subject, mail_template, context)
+
+                else:
+                    #send notification email
+                    mail_subject = "We're sorry! YOu are not eligible for your menu on our marketplace"
+                    send_notification(mail_subject, mail_template, context)
+        return super(Vendor, self).save(*args, **kwargs)
