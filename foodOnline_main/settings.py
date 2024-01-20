91ada1f7e4c1a4e76ce957a69d738556d25ce72b
yutaku-kim <yutaku1011@gmail.com>
2024-01-20 17:53:54 +0900

    location based search functionalities

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -32,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.gis',
     'marketplace',
     'menu',
     'vendor',
@@ -84,7 +86,8 @@ WSGI_APPLICATION = 'foodOnline_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        #'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
@@ -163,3 +166,8 @@ DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <yutaku1011@gmail.com>'
 
 
 GOOGLE_API_KEY = 'AIzaSyCYFTkTcRWsIdx_wgEt5q3sXBho4uDv_p4'
+
+
+os.environ['PATH'] = os.path.join(BASE_DIR, 'env/Lib/site-packages/osgeo') + ';' + os.environ['PATH']
+os.environ['PROJ_LIB'] = os.path.join(BASE_DIR, 'env/Lib/site-packages/osgeo/data/proj') + ';' + os.environ['PATH']
+GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, 'env/Lib/site-packages/osgeo/gdal304.dll')
--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('marketplace/', include('marketplace.urls')),
     path('cart/', MarketplaceViews.cart, name='cart'),
+    # search
+    path('search/', MarketplaceViews.search, name='search'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
