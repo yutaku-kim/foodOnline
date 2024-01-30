d0844b416e84affa35ccebc02c386823069c63e4
yutaku-kim <yutaku1011@gmail.com>
2024-01-30 15:59:35 +0900

    customer dashboad

--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.tokens import default_token_generator
 from vendor.models import Vendor
-
+from orders.models import Order
 from django.template.defaultfilters import slugify
 # Restrict the vendor from accessing the customer page
 def check_role_vendor(user):
@@ -175,7 +175,15 @@ def myAccount(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_customer)
 def custDashboard(request):
-    return render(request, 'accounts/custDashboard.html')
+    orders = Order.objects.filter(user=request.user)
+    recent_orders = orders[:5]
+    
+    context = {
+        'orders': orders,
+        'orders_count': orders.count(),
+        'recent_orders': recent_orders,
+    }
+    return render(request, 'accounts/custDashboard.html', context)
 
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
